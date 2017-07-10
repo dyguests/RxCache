991a992f8fff4931fa5e52ac4d45e068ea43cc8d
fanhl <fanhonglin@klicen.com>
2017-07-10 18:10:53 +0800

    test

--- app/src/test/java/com/fanhl/rxcache/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/fanhl/rxcache/sample/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.fanhl.rxcache.sample
 
+import io.reactivex.Observable
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,8 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+
+        Observable.just("dummy data")
+                .subscribe { print(it) }
     }
 }
