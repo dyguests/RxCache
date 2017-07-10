02f49e316a676a9aec34433a24411947a0352900
fanhl <fanhonglin@klicen.com>
2017-07-10 19:38:52 +0800

    main

--- app/src/main/java/com/fanhl/rxcache/sample/MainActivity.kt
+++ app/src/main/java/com/fanhl/rxcache/sample/MainActivity.kt
@@ -3,14 +3,13 @@ package com.fanhl.rxcache.sample
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-import kotlinx.android.synthetic.main.activity_test.*
-
-class TestActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG).show() }
