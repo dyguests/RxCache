55c1db43ac537e66ef76ffc2a9100d3eb1936f01
fanhl <fanhonglin@klicen.com>
2017-07-11 09:46:16 +0800

    cache

---
+++ app/src/main/java/com/fanhl/rxcache/sample/App.kt
@@ -0,0 +1,18 @@
+package com.fanhl.rxcache.sample
+
+import android.app.Application
+import com.fanhl.rxcache.RxCache
+
+/**
+ * desc:
+ * date: 2017/7/11
+
+ * @author fanhl
+ */
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        RxCache.init(this)
+    }
+}
--- library/src/main/java/com/fanhl/rxcache/RxCache.kt
+++ library/src/main/java/com/fanhl/rxcache/RxCache.kt
@@ -1,5 +1,6 @@
 package com.fanhl.rxcache
 
+import android.content.Context
 import com.google.gson.reflect.TypeToken
 import io.reactivex.ObservableTransformer
 
@@ -10,17 +11,23 @@ import io.reactivex.ObservableTransformer
  * @author fanhl
  */
 object RxCache {
-    val provider: ICacheProvider = DefaultCacheProvider()
+    var provider: ICacheProvider? = null
+
+    fun init(context: Context) {
+//        provider = DefaultCacheProvider(context)
+    }
 
     fun <T> cache(
             name: String,
             vararg conditions: String
     ) = ObservableTransformer<T, T> { upStream ->
+        checkInit()
+
         val key = name + "-" + conditions.joinToString("-")
 
         var currStream = upStream.map { CacheWrap(it) }
 
-        val cacheWrap: CacheWrap<T>? = provider.get(key, object : TypeToken<CacheWrap<T>>() {}.type)
+        val cacheWrap: CacheWrap<T>? = provider!!.get(key, object : TypeToken<CacheWrap<T>>() {}.type)
 
         cacheWrap?.let {
             //以防doOnNext时进行重复缓存
@@ -30,8 +37,17 @@ object RxCache {
 
         currStream.doOnNext {
             if (it.type != CacheWrap.Type.FromCache) {
-                provider.put(key, it)
+                provider!!.put(key, it)
             }
         }.map { it.data }
+    }
+
+    private fun checkInit() {
+        if (provider == null) {
+            throw Exception("""Provider not init.
+may be you forget call RxCache.init().
+Remember call init() before cache().
+Please call init() in YourApplication.""")
+        }
     }
 }
